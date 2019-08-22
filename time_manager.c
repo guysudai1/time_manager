97123248de19efe8b747976ca604afb99ca4490e
guysudai1 <sudaiguy1@gmail.com>
2019-08-22 13:19:32 +0300

    Finished schedule printing + add events works now

--- time_manager.c
+++ time_manager.c
@@ -10,6 +10,7 @@ This program creates files with a schedule.
 
 #define EVENTS_PER_DAY 10
 #define DAYS_PER_WEEK  7
+#define EVENT_NAME_SIZE 20
 
 typedef struct {
 	char name[20];
@@ -34,7 +35,7 @@ Event* get_new_event() {
 	
 	// Day variable consists of a number from 0-6(including 6) and represents sunday-saturday
 	int day, hour, minute;
-	char name[20];
+	char name[EVENT_NAME_SIZE];
 	name[0] = 0;
 	printf("Day hour minute of event: ");
 	scanf("%1d %2d %2d", &day, &hour, &minute);
@@ -42,7 +43,7 @@ Event* get_new_event() {
 	if (day > 6 || hour > 24 || minute > 60) return NULL;
 	
 	// Name of new event
-	printf("Name of the event(max 20): ");
+	printf("Name of the event(max %d): ", EVENT_NAME_SIZE);
 	scanf("%20s", name);
 	if (strlen(name) == 0) return NULL;
 	
@@ -55,16 +56,106 @@ Event* get_new_event() {
 	return event;
 }	
 
+int compare_events(Event* event1, Event* event2) {
+	// Compares events by their members
+	return (strcmp(event1->name, event2->name) && event1->day == event2->day && event1->hour == event2->hour && event1->minute == event2->minute) ? 1 : 0;
+}
+
+int binary_search(Event* day[EVENTS_PER_DAY], int low, int high, Event* event) {
+	// Finds event in a day and returns the index, if doesn't exist then return -1.
+
+	if (high < low) return -1;
+	int middle = (high + low) / 2;
+	Event* middle_event = day[middle];
+	if (compare_events(middle_event, event)) return middle;
+	
+	// Check hour
+	if (middle_event->hour > event->hour) return binary_search(day, low, middle, event);
+	if (middle_event->hour < event->hour) return binary_search(day, middle+1, high, event);
+
+	// Same hour - Check minute
+	if (middle_event->minute > event->hour) return binary_search(day, low, middle, event);
+	return binary_search(day, middle+1, high, event);
+}
+
+int enter_sorted(Event* schedule[DAYS_PER_WEEK][EVENTS_PER_DAY], int schedule_index[DAYS_PER_WEEK], Event* event) {
+	int event_day = event->day;
+	if (schedule_index[event_day] == 0) {
+		schedule[event_day][0] = event;
+		return 1;
+	}
+	// check if array is maxed out
+	if (schedule_index[event_day] == EVENTS_PER_DAY) return 0;
+
+	// Find appropriate index to add event in.
+	int i;
+	for (i=schedule_index[event_day] - 1; i >= 0; i--) {
+		Event* event_i = schedule[event_day][i];
+		if (event_i->hour <= event->hour && event_i->minute < event->minute) 
+			break;
+		// shift items to the right
+		schedule[event_day][i+1] = schedule[event_day][i];
+	}
+	schedule[event_day][i+1] = event;
+	return 1;
+}
+int max(int schedule_index[DAYS_PER_WEEK]) {
+	int max_day = schedule_index[0];
+	for (int i=1; i < DAYS_PER_WEEK; i++) 	{
+		int current_value = schedule_index[i];
+		if (current_value > max_day) max_day = current_value;
+	}
+	return max_day;
+}
+
 void print_schedule(Event* schedule[DAYS_PER_WEEK][EVENTS_PER_DAY], int schedule_index[DAYS_PER_WEEK]) {
-	// this program prints out the schedule by order of time, from lowest to heighest.
-	char* days = {"Sunday", "Monday", "Tuesday", "Wedensday", "Thursday", "Friday", "Saturday"};
-	char* day_bar[23*sizeof(days)+ 4] = "DAY |";
-	for (int i=0; i<sizeof(days); i++) {
-		char* day = days[i];
-		int spaces_before_and_after = (strlen(day) % 2 == 0) ? (22 - strlen(day)) / 2: (23 - strlen(day)) / 2;
-		// TODO : Add string like "         Sunday          " to days bar.
-	}											      
-	printf("DAY |                      |"
+	// Prints out the schedule by order of time, from lowest to heighest. 
+	
+	char line_break[] = "|====================|====================|====================|====================|====================|====================|====================|";
+	printf("\n|       Sunday       |       Monday       |       Teusday      |      Wedensday     |      Thursday      |       Friday       |      Saturday      |\n%s\n",line_break);
+	//printf("%s\n", line_break);
+	//printf("break before function\n");	
+	//printf("Max value: %d\n", max(schedule_index));
+	
+	for (int i=0; i < max(schedule_index);i++) {
+		char events[8+EVENT_NAME_SIZE*7 + 1] = {0};
+		char  hours[8+EVENT_NAME_SIZE*7 + 1] = {0};
+		strcat(events, "|");
+		strcat(hours,  "|");
+		for (int day=0; day < DAYS_PER_WEEK; day++) {
+			char temp_events[EVENT_NAME_SIZE + 1]= {0}; 
+			char temp_hours[EVENT_NAME_SIZE + 1] = {0};
+			for (int i=0; i<EVENT_NAME_SIZE;i++) {temp_events[i] = ' '; temp_hours[i] = ' ';}
+
+
+			//if (schedule_index[day] >= i + 1) printf("\nString: %s, String size: %zu, schedule-index: %d\n",schedule[day][i]->name, strlen(schedule[day][i]->name), schedule_index[day]);
+			if (schedule_index[day] >= i + 1) {
+				char* event_name = schedule[day][i]->name;
+				int event_name_size = strlen(event_name);
+				strncpy(&temp_events[(EVENT_NAME_SIZE + 1-event_name_size)/2], event_name, event_name_size);  // names[day]=schedule[day][i];
+			
+				
+				char* temp_str;
+				sprintf(temp_str, "%02d:%02d", schedule[day][i]->hour, schedule[day][i]->minute);
+
+				int hour_size = strlen(temp_str);
+				strncpy(temp_hours + (EVENT_NAME_SIZE - 4)/2,temp_str, hour_size);
+			}
+			strcat(events, temp_events);
+			strcat(events, "|");
+
+			strcat(hours, temp_hours);
+			strcat(hours, "|");
+		}
+
+		
+		//printf("%d - %s",i,line_break);
+		
+		printf("%s\n", events);
+		printf("%s\n", hours);
+		printf("%s\n", line_break);
+		
+	}
 }	
 
 
@@ -101,8 +192,13 @@ int main() {
 					free(event);
 					break;
 				}
-				schedule[event->day + DAYS_PER_WEEK * event_num] = *event;
-				schedule_index[event->day]++;
+				
+				if (enter_sorted(schedule, schedule_index, event)) {
+					printf("[+] Successfully added new event to schedule!");
+					schedule_index[event->day]++;
+				} else { printf("Couldn't add new event to schedule!"); }
+				
+				print_schedule(schedule, schedule_index);
 				break;
 			case 2:
 				// TODO : Create option to delete events from schedule.
